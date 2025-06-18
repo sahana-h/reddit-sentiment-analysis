33d8af7ebad87c8b2e908e0b0ea140bfd0073739
Sahana Hariharan <sahanahariharan@gmail.com>
2025-06-18 13:59:45 -0500

    initial commit with basic Streamlit app

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="RetailRadar", layout="wide", initial_sidebar_state="expanded")
+
+st.title("RetailRadar: Real-Time Reddit Sentiment Dashboard for Stocks")
+st.write("Welcome! This dashboard will let you explore Reddit sentiment for your favorite stocks in real time.")
